6bc26378296b025fa9f6735494efbfcd85c600ab
Valeriya_Chernykh <jamarks@mail.ru>
2024-06-06 19:58:27 +0300

    Commit for homework

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -10,10 +10,10 @@ def test_check_icon(browser):
 def test_check_field1(browser):
     swag_labs_page = SwagLabs(browser)
     swag_labs_page.visit()
-    assert swag_labs_page.exist_field_name('#user-name')
+    assert swag_labs_page.exist_field('#user-name')
 
 
 def test_check_field2(browser):
     swag_labs_page = SwagLabs(browser)
     swag_labs_page.visit()
-    assert swag_labs_page.exist_field_name('#password')
+    assert swag_labs_page.exist_field('#password')
